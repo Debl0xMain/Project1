f66506ddda4718d0e43f56d27433f79399a30c85
Selli <Selli@DESKTOP-SK8ATS2>
2023-12-12 14:14:28 +0100

    Ajoutez des fichiers projet.

---
+++ Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Project1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
